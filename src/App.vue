46e27226de87b6ef1cb348fc8cebf96bbfc02410
Fan Yang <fanyang@Fans-MBP.attlocal.net>
2020-07-17 00:55:30 -0500

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <navbar ref='mynavbar'></navbar>
+    <tableview ref="mytableview"></tableview>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import navbar from './components/Navbar'
+import tableview from './components/Tableview'
 
-#nav {
-  padding: 30px;
+export default {
+  data: function () {
+    return {
+      userinput: '',
+      websites: ['https://api03.6bqb.com/alibaba/app/search']
+    }
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    navbar: navbar,
+    tableview: tableview
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
+  methods: {
+    update_search: function () {
+      this.userinput = this.$refs.mynavbar.getUserInput()
+      console.log(this.$refs.mynavbar.getUserInput())
+    },
+
+    sortbyprice: function () {
+
     }
   }
 }
-</style>
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <bootstrap/>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" @click="updatePriceList">Pricing/价格排行</a>
+            </li>
+          </ul>
+          <div class="form-check-inline">
+            <input v-model="userinput" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button @click="updateSearch" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </div>
+        </div>
+      </nav>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.nav-item{
+  margin-left: 50px;
+}
+
+.form-check-inline{
+  margin-left: 50px;
+}
+</style>
+
+<script>
+import bootstrap from './stylesheet'
+import { bus } from '../main.js'
+
+export default {
+  data: function () {
+    return {
+      userinput: ''
+    }
+  },
+
+  methods: {
+    updatePriceList: function () {
+      bus.$emit('sortByPrice')
+    },
+
+    updateSearch: function () {
+      bus.$emit('searchItem', this.userinput)
+    }
+
+  },
+  components: {
+    bootstrap: bootstrap
+  }
+}
+</script>
